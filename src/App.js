053d45122086cc0f80ddd501b44edbaf9ed05659
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2022-04-03 19:14:17 +0430

    add simple toggle component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import Toggle from "./components/Toggle";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ToggleValue, setToggleValue] = useState(true);
+    const [ToggleSmValue, setToggleSmValue] = useState(false);
+
+    return (
+      <section className={'flex justify-center py-5'}>
+            <div className={'flex-center bg-gray-25 w-[90%] h-[160px] rounded-xl p-4'}>
+                <div className={'flex-col-center-y items-start w-[200px]'}>
+                    <div className={'space-x-2 flex-center'}>
+                        <Toggle value={ToggleValue} onCheckedChange={(val) => setToggleValue(val)} />
+                        <span>
+                            Toggle
+                        </span>
+                    </div>
+                    <div className={'space-x-2 flex-center'}>
+                        <Toggle value={ToggleSmValue} onCheckedChange={(val) => setToggleSmValue(val)} sm />
+                        <span>
+                            Toggle sm
+                        </span>
+                    </div>
+                </div>
+            </div>
+      </section>
+    );
 }
 
 export default App;
---
+++ src/components/Toggle/index.js
@@ -0,0 +1,55 @@
+import clsx from 'clsx';
+import * as Switch from "@radix-ui/react-switch";
+
+function Toggle({ sm, value, ...otherProp }) {
+    return (
+        <div className={getContainerClass(value, sm)}>
+            <Switch.Root
+                value={value}
+                className={getSwitchClass(value, sm)}
+                {...otherProp}
+            >
+                <Switch.Thumb className={getThumbClass(value, sm)} />
+            </Switch.Root>
+        </div>
+    );
+}
+
+function getThumbClass (val, sm) {
+    const baseClass = 'bg-white block z-10 rounded-full translate-x-4 transition duration-150 ease-out';
+
+    return clsx(baseClass, {
+        'w-5 h-3': sm,
+        'w-6 h-4': !sm,
+        'translate-x-1': val
+    });
+}
+
+function getSwitchClass (val, sm) {
+    const baseClass = 'rounded-full relative z-10';
+
+    return clsx(baseClass, {
+        'w-10 h-4': sm,
+        'w-11 h-5': !sm,
+        'bg-primary-500': val,
+        'bg-gray-200': !val,
+    });
+}
+
+function getContainerClass (val, sm) {
+    const baseClass = 'flex-center rounded-full transition duration-150 ease-out';
+
+    return clsx(baseClass, {
+        'w-11 h-5': sm,
+        'w-12 h-6': !sm,
+        'hover:bg-primary-200': val,
+        'hover:bg-gray-50': !val,
+    });
+}
+
+
+Toggle.defaultProps = {
+    sm: false,
+    value: false,
+};
+export default Toggle;
